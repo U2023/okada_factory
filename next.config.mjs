fb0dd663770b22259475a2d1c1d465d33be6d0dd
U2023 <kaedemau@outlook.com>
2025-04-16 21:44:43 +0900

    nextConfigにbasePath追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // HTML出力モード
+  basePath: "/okada_factory/",
   trailingSlash: true, //url末尾に/をつける
   images: {
     unoptimized: true, //画像最適化を無視（SGでは不可）
